977bf05e8d561254dea9042ff771b948a1df0d68
somu-ssv <100763930+somu-ssv@users.noreply.github.com>
2022-03-22 12:15:15 +0530

    Add files via upload

---
+++ VehicleEmployementDetailsApplication.java
@@ -0,0 +1,14 @@
+package com.lti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleEmployementDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleEmployementDetailsApplication.class, args);
+		System.out.println("EMPLOYEMENT DETAILS IS SUCCESS....");
+	}
+
+}
